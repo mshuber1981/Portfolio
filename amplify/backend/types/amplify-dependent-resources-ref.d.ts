fa085d6e1adcaa303a7066aa13e99a3eac78ffe9
mshuber1981 <michael.huber1981@outlook.com>
2023-06-02 15:17:57 -0500

    Removing test api/lambda

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,18 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "contactForm": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
-    }
-  },
-  "function": {
-    "portfolioContactForm": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
-    }
-  }
-}
+export type AmplifyDependentResourcesAttributes = {}
